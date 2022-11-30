a8c2912d1b0f64f5be01fad0782317f0fa9fd096
Reedraw <63612187+Reedraw@users.noreply.github.com>
2022-11-30 22:28:48 +0000

    Updated MIC INPUT
    User doesn't have to keep clicking the input box

--- public/js/micinput.js
+++ public/js/micinput.js
@@ -3,27 +3,33 @@ function record(){
     recognition.lang = "en-GB";
     recognition.continuous = true;
     
+    var x = 0;
+
     recognition.onresult = function(event){
         console.log(event);
-        console.log(event.results[0][0].transcript);
-        document.getElementById('speechToText').value = event.results[0][0].transcript
+        console.log(event.results[event.results.length-1][0].transcript);
+        document.getElementById('speechToText').value = event.results[event.results.length-1][0].transcript
 
-        var micresults = event.results[0][0].transcript
+        var micresults = event.results[event.results.length-1][0].transcript.trim()
+        console.log(`Here: ${micresults}`)
         if (micresults.toLowerCase() == "green"){
-            console.log("Green")
+            console.log("Green1")
             document.getElementById("answer1").click()
         } else if (micresults.toLowerCase() == "red"){
-            console.log("Red")
+            console.log("Red1")
             document.getElementById("answer2").click()
         } else if (micresults.toLowerCase() == "blue"){
-            console.log("Blue")
+            console.log("Blue1")
             document.getElementById("answer3").click()
         } else if (micresults.toLowerCase() == "orange"){
-            console.log("Orange")
+            console.log("Orange1")
             document.getElementById("answer4").click()
         }
 
     }
 
     recognition.start();
+    x += 1;
+
+    
 }
